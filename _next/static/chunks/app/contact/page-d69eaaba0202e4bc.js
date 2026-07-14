e5180631b4d0332b6f04038ec73479ee4b04c6b0
varalakshmivaru231020-svg <kishorreddy992@gmail.com>
2026-07-14 22:49:31 +0530

    Static build for live deployment

---
+++ _next/static/chunks/app/contact/page-d69eaaba0202e4bc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[786,904,977],{5967:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[619,441,255,358],()=>e(e.s=5967)),_N_E=e.O()}]);
